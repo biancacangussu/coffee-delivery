f146b2da1a96a1f02923aeec97d1333b04077c0d
Bianca Cangussu <biancaaltex@gmail.com>
2025-08-20 19:24:20 -0300

    chore: setup theme and global styles configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import { ThemeProvider } from "styled-components"
+import { defaultTheme } from "./styles/theme/default"
+import { GlobalStyle } from "./styles/global"
+
 function App() {
-  return <div></div>;
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+      <h1>Coffee Delivery</h1>
+    </ThemeProvider>
+  )
 }
 
-export default App;
+export default App
