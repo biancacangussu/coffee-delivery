e8c9ba272a4db30244b02b7cc21989fb592fac5e
Bianca Cangussu <biancaaltex@gmail.com>
2025-08-22 17:23:19 -0300

    feat: changes in the QuantityInput component

--- src/components/QuantityInput/index.tsx
+++ src/components/QuantityInput/index.tsx
@@ -3,24 +3,18 @@ import { Minus, Plus } from "phosphor-react";
 
 interface QuantityInputProps {
   size?: "medium" | "small";
-  quantity: number;
-  onIncrease: () => void;
-  onDecrease: () => void;
 }
 
 export function QuantityInput({
-  onIncrease,
-  onDecrease,
-  quantity,
   size = "medium",
 }: QuantityInputProps) {
   return (
     <QuantityInputContainer size={size}>
-      <IconWrapper onClick={onDecrease} disabled={quantity <= 1}>
+      <IconWrapper>
         <Minus size={14} weight="fill" />
       </IconWrapper>
-      <input type="number" readOnly value={quantity} />
-      <IconWrapper onClick={onIncrease}>
+      <input type="number" readOnly value={1} />
+      <IconWrapper>
         <Plus size={14} weight="fill" />
       </IconWrapper>
     </QuantityInputContainer>
