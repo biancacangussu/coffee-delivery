982de1ba504000b1826c8ebf8ce5248d71be21f5
Bianca Cangussu <biancaaltex@gmail.com>
2025-09-23 20:30:41 -0300

    feat: add address autocomplete api for the cep input

--- src/pages/CompleteOrder/components/CompleteOrderForm/AddressForm.tsx
+++ src/pages/CompleteOrder/components/CompleteOrderForm/AddressForm.tsx
@@ -1,6 +1,7 @@
 import { useFormContext } from "react-hook-form";
 import { Input } from "../../../../components/Input";
 import { AddressFormContainer } from "./styles";
+import { type ChangeEvent, type KeyboardEvent } from "react";
 
 interface ErrorsType {
   errors: {
@@ -11,18 +12,56 @@ interface ErrorsType {
 }
 
 export function AddressForm() {
-  const { register, formState } = useFormContext();
-
+  const { register, formState, watch, setValue } = useFormContext();
   const { errors } = formState as unknown as ErrorsType;
+
+  const cep = watch("cep");
+
+  const handleSearchCEP = async () => {
+    try {
+      const onlyNumbers = cep?.replace(/\D/g, "");
+      if (onlyNumbers?.length === 8) {
+        const res = await fetch(`https://viacep.com.br/ws/${onlyNumbers}/json/`);
+        const data = await res.json();
+
+        if (!data.erro) {
+          setValue("street", data.logradouro || "");
+          setValue("district", data.bairro || "");
+          setValue("city", data.localidade || "");
+          setValue("uf", data.uf || "");
+        }
+      }
+    } catch (error) {
+      console.log("Erro ao buscar CEP", error);
+    }
+  };
+
+  const handleCepChange = (e: ChangeEvent<HTMLInputElement>) => {
+    let value = e.target.value.replace(/\D/g, "");
+    if (value.length > 5) {
+      value = value.replace(/^(\d{5})(\d{1,3}).*/, "$1-$2");
+    }
+    setValue("cep", value);
+  };
+
+  const handleKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      e.preventDefault();
+      handleSearchCEP();
+    }
+  };
 
   return (
     <AddressFormContainer>
       <Input
         placeholder="CEP"
-        type="number"
+        type="text"
         className="cep"
         {...register("cep")}
         error={errors.cep?.message}
+        onBlur={handleSearchCEP}
+        onChange={handleCepChange}
+        onKeyDown={handleKeyDown}
       />
       <Input
         placeholder="Rua"
@@ -53,7 +92,12 @@ export function AddressForm() {
         {...register("city")}
         error={errors.city?.message}
       />
-      <Input placeholder="UF" {...register("uf")} error={errors.uf?.message} />
+      <Input
+        placeholder="UF"
+        maxLength={2}
+        {...register("uf")}
+        error={errors.uf?.message}
+      />
     </AddressFormContainer>
   );
 }
